7f38efb104b33aca3efb39b4250b59aeae936779
yumeji <xzrider4589@gmail.com>
2024-11-29 11:01:02 -0600

    adding the new staic page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
         <h1>Hello this is my first react app!</h1>
+        <p>
+          This is just a paragraph letting you know that there are 
+          some changes that happened while working on the project 4.
+        </p>
     </div>
   );
 }
