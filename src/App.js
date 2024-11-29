84f8e06850ff8c614084e1d758cbfe6a8a92a479
yumeji <xzrider4589@gmail.com>
2024-11-29 10:22:21 -0600

    adding the new yml file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Madison, WI!!!! 'w'</h1>
+        <h1>Hello this is my first react app!</h1>
     </div>
   );
 }
