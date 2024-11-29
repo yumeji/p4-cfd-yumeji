a5d5bfb59f740800ba9fe6025e12c26edf159433
yumeji <xzrider4589@gmail.com>
2024-11-29 11:15:34 -0600

    updating the web page for the index page on s3 bucket

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ function App() {
   return (
     <div className="App">
         <h1>Hello this is my first react app!</h1>
+        <h2>My name is: Jose Tecuatl Cortes, let me tell you about myself.</h2>
         <p>
-          This is just a paragraph letting you know that there are 
-          some changes that happened while working on the project 4.
-          ewsfiopgfggfklndjkdkfsgkghfkfjdlsfsddfgjdhifd
-          d m grea gjadsr ipgjadsl gjoasdfa
-          sd
-          dsaf sd jfdsao fdpsajfpdsaj fdsaewj fwe 
-          ewgwerkgero ger igjiorewjg irejgergerwgrewgjrewogejorgjoj
-          rgepjjeropgjoregjoprejgrepojgrejregopjergop
+          This page involves three different components:
+        </p>
+        <ul>
+          <li>One of which is a github repository connected to a windows computer</li>
+          <li>Then an AWS s3 bucket hosting a static web page.</li>
+          <li>Last but not least is the codepipeline, allowing any git changes to the webpage</li>
+        </ul>
+        <p>
+          Overall if I were to make this some sort of application using git respository
+          I wouldn't use a windows machine because using git on windowsis a bit more combersome
+          compared to the much more complex but convinent linux distribution. I defintely
+          would give ubuntu as a great recommendation for starters. 
         </p>
     </div>
   );
